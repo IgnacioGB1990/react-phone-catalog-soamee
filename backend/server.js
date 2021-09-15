7e8a6c193a8cde2770ee27a516e205d9959ced5b
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2021-09-15 19:23:54 +0200

    Connected Back to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express"
 import dotenv from "dotenv"
+import connectDB from "./config/db.js"
 import phones from "./data/phones.js"
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
