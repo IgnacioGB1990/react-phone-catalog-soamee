5169d4bda9d5dd418a4dcedbc4ef6ea8edfd3bba
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2021-09-15 16:51:05 +0200

    Fetching phones from Back

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Phone from "../components/Phone"
+import axios from "axios"
 import Loader from "../components/Loader"
 import "../styles/HomeScreen.css"
-import phones from "../data/phones"
 
 const HomeScreen = () => {
 
+  const [phones, setPhones] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchPhones = async () => {
+      const { data } = await axios.get("/api/phones")
+      setPhones(data)
+      setLoading(false)
+    }
+
+    fetchPhones()
+  }, [])
+
   console.log(phones)
-  return (
-    <div className="homeSectionContainer">
+  return (<div>
+
+
+    { loading && <Loader />}
+    {!loading && <div className="homeSectionContainer">
       {phones.map((phone, index) => (
         <li className="phoneList" key={index}>
           <Phone phone={phone} />
 
         </li>
       ))}
-    </div>
+    </div>}
+  </div>
   )
 }
 
--- frontend/src/screens/PhoneScreen.js
+++ frontend/src/screens/PhoneScreen.js
@@ -1,37 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from "axios"
+import Loader from "../components/Loader"
 import "../styles/PhoneScreen.css"
-import phones from "../data/phones"
 
 const PhoneScreen = ({ match }) => {
 
-  const phone = phones.find((p) => p._id === match.params.id)
+  const [phone, setPhone] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchPhone = async () => {
+      const { data } = await axios.get(`/api/phones/${match.params.id}`)
+      setPhone(data)
+      setLoading(false)
+    }
+    fetchPhone()
+  }, [match])
 
   return (
-    <div className="phoneScreenContainer">
+    <>
+      { loading && <Loader />}
+      <div className="phoneScreenContainer">
 
-      <img src={phone.imageFileName} alt={phone.name} />
-
-
-
-      <div >Name:</div>
-      <p>{phone.name}</p>
-      <div>Manufacturer:</div>
-      <p>{phone.manufacturer}</p>
-      <div>Description:</div>
-      <p>{phone.description}</p>
-      <div>Color:</div>
-      <p>{phone.color}</p>
-      <div>Price:</div>
-      <p>{phone.price}</p>
-      <div>Screen:</div>
-      <p>{phone.screen}</p>
-      <div>Processor:</div>
-      <p>{phone.processor}</p>
-      <div>Ram:</div>
-      <p>{phone.ram}</p>
-    </div>
+        <img src={phone.imageFileName} alt={phone.name} />
 
 
+
+        <div >Name:</div>
+        <p>{phone.name}</p>
+        <div>Manufacturer:</div>
+        <p>{phone.manufacturer}</p>
+        <div>Description:</div>
+        <p>{phone.description}</p>
+        <div>Color:</div>
+        <p>{phone.color}</p>
+        <div>Price:</div>
+        <p>{phone.price}</p>
+        <div>Screen:</div>
+        <p>{phone.screen}</p>
+        <div>Processor:</div>
+        <p>{phone.processor}</p>
+        <div>Ram:</div>
+        <p>{phone.ram}</p>
+      </div>
+
+    </>
   )
 }
 
